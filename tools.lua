ca51ce1a75010e7458776cfaecc2bb83a6e39efa
benedict42 <pianoj@ymail.com>
2014-11-05 11:55:46 -0800

    Update tools.lua

--- tools.lua
+++ tools.lua
@@ -1,190 +1,69 @@
---Alchemical Tinkerer Mod
---By Benedict42
 
-alchemtinker_modpath = minetest.get_modpath("alchemtinker")
-dofile(alchemtinker_modpath .. "/plants.lua")
-dofile(alchemtinker_modpath .. "/tools.lua")
-dofile(alchemtinker_modpath .. "/wands.lua")
-dofile(alchemtinker_modpath .. "/scrolls.lua")
-
-minetest.register_craftitem("alchemtinker:terra_essence", {
-	description = "Terra Essence",
-	inventory_image = "tinker_terra_essence.png",
-	stack_max = 999,
+-----Terra Mattock-----
+minetest.register_tool("alchemtinker:terra_mattock", {
+	description = "Terra Mattock",
+	inventory_image = "tinker_terra_mattock.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=3,
+		groupcaps={
+			cracky={times={[1]=2.0, [2]=1.0, [3]=0.5}, uses=20, maxlevel=3},
+			crumbly={times={[1]=1.20, [2]=0.60, [3]=0.30}, uses=20, maxlevel=3},
+			snappy={times={[1]=2.0, [2]=1.0, [3]=0.5}, uses=20, maxlevel=3},
+			choppy={times={[1]=2.20, [2]=1.00, [3]=0.60}, uses=20, maxlevel=3},
+		},
+	},
 })
 
-minetest.register_craftitem("alchemtinker:aqua_essence", {
-	description = "Aqua Essence",
-	inventory_image = "tinker_aqua_essence.png",
-	liquids_pointable = true,
-	stack_max = 999,
-})
-
-minetest.register_craftitem("alchemtinker:air_essence", {
-	description = "Air Essence",
-	inventory_image = "tinker_air_essence.png",
-	stack_max = 999,
-})
-
-minetest.register_craftitem("alchemtinker:ignus_essence", {
-	description = "Ignus Essence",
-	inventory_image = "tinker_ignus_essence.png",
-	stack_max = 999,
-})
-
-minetest.register_craftitem("alchemtinker:incognitus_essence", {
-	description = "Incognitus Essence",
-	inventory_image = "tinker_incognitus_essence.png",
-	stack_max = 999,
-})
-
---[[
 minetest.register_craft({
-	type = "shapeless",
-	output = "alchemtinker:light_node 100",
+	output = "alchemtinker:terra_mattock",
 	recipe = {
-		"alchemtinker:ignus_essence", "alchemtinker:air_essence"
+		{"alchemtinker:terra_essence", "alchemtinker:terra_essence", "alchemtinker:terra_essence"},
+		{"", "alchemtinker:terra_essence", "alchemtinker:terra_essence"},
+		{"", "alchemtinker:terra_essence", ""}
 	}
 })
---]]
-----------------
 
---[[
-minetest.register_node("alchemtinker:alchemy_furnace", {
-	description = "Alchemy Furnace",
-	tiles ={"default_furnace_side.png", "default_furnace_side.png", "default_furnace_side.png",
-		"default_furnace_side.png", "default_furnace_side.png", "default_furnace_front.png"},
-	paramtype2 = "facedir",
-	groups = {cracky=2},
-	legacy_facedir_simple = true,
-	is_ground_content = false,
-	sounds = default.node_sound_stone_defaults(),
-	on_construct = function(pos)
-		local meta = minetest.get_meta(pos)
-		meta:set_string("formspec", default.furnace_inactive_formspec)
-		meta:set_string("infotext", "Alchemy Furnace")
-		local inv = meta:get_inventory()
-		inv:set_size("fuel", 1)
-		inv:set_size("src", 1)
-		inv:set_size("dst", 4)
-	end,
-	can_dig = function(pos,player)
-		local meta = minetest.get_meta(pos);
-		local inv = meta:get_inventory()
-		if not inv:is_empty("fuel") then
-			return false
-		elseif not inv:is_empty("dst") then
-			return false
-		elseif not inv:is_empty("src") then
-			return false
-		end
-		return true
-	end,
+-----Aqua Plogh----- This item uses an unusual API.
+farming.register_hoe("alchemtinker:aqua_plogh", {
+	description = "Aqua Plogh",	-- Description for tooltip
+	inventory_image = "tinker_aqua_plogh.png",	-- Image to be used as wield- and inventory image
+	max_uses = 350,	-- Uses until destroyed
+	recipe = {	-- Craft recipe
+		{"tinkerardcraft:aqua_essence", "tinkerardcraft:aqua_essence"},
+		{"", "tinkerardcraft:aqua_essence"},
+		{"", "tinkerardcraft:aqua_essence"},
+	}
 })
 
------
-
-minetest.register_abm({
-	nodenames = {"alchemtinker:alchemy_furnace"},
-	interval = 1.0,
-	chance = 1,
-	action = function(pos, node, active_object_count, active_object_count_wider)
-		local meta = minetest.get_meta(pos)
-		for i, name in ipairs({
-				"fuel_totaltime",
-				"fuel_time",
-				"src_totaltime",
-				"src_time"
-		}) do
-			if meta:get_string(name) == "" then
-				meta:set_float(name, 0.0)
-			end
-		end
-
-		local inv = meta:get_inventory()
-
-		local srclist = inv:get_list("src")
-		local cooked = nil
-		
-		if srclist then
-			cooked = minetest.get_craft_result({method = "cooking", width = 1, items = srclist})
-		end
-		
-		local was_active = false
-		
-		if meta:get_float("fuel_time") < meta:get_float("fuel_totaltime") then
-			was_active = true
-			meta:set_float("fuel_time", meta:get_float("fuel_time") + 1)
-			meta:set_float("src_time", meta:get_float("src_time") + 1)
-			if cooked and cooked.item and meta:get_float("src_time") >= cooked.time then
-				-- check if there's room for output in "dst" list
-				if inv:room_for_item("dst",cooked.item) then
-					-- Put result in "dst" list
-					inv:add_item("dst", cooked.item)
-					-- take stuff from "src" list
-					srcstack = inv:get_stack("src", 1)
-					srcstack:take_item()
-					inv:set_stack("src", 1, srcstack)
-				else
-					print("Could not insert '"..cooked.item:to_string().."'")
-				end
-				meta:set_string("src_time", 0)
-			end
-		end
-		
-		if meta:get_float("fuel_time") < meta:get_float("fuel_totaltime") then
-			local percent = math.floor(meta:get_float("fuel_time") /
-					meta:get_float("fuel_totaltime") * 100)
-			meta:set_string("infotext","Furnace active: "..percent.."%")
-			swap_node(pos,"default:furnace_active")
-			meta:set_string("formspec",
-				"size[8,9]"..
-				"image[2,2;1,1;default_furnace_fire_bg.png^[lowpart:"..
-						(100-percent)..":default_furnace_fire_fg.png]"..
-				"list[current_name;fuel;2,3;1,1;]"..
-				"list[current_name;src;2,1;1,1;]"..
-				"list[current_name;dst;5,1;2,2;]"..
-				"list[current_player;main;0,5;8,4;]")
-			return
-		end
-
-
-		local fuel = nil
-		local cooked = nil
-		local fuellist = inv:get_list("fuel")
-		local srclist = inv:get_list("src")
-		
-		if srclist then
-			cooked = minetest.get_craft_result({method = "cooking", width = 1, items = srclist})
-		end
-		if fuellist then
-			fuel = minetest.get_craft_result({method = "fuel", width = 1, items = fuellist})
-		end
-
-		if fuel.time <= 0 then
-			meta:set_string("infotext","Furnace out of fuel")
-			swap_node(pos,"default:furnace")
-			meta:set_string("formspec", default.furnace_inactive_formspec)
-			return
-		end
-
-		if cooked.item:is_empty() then
-			if was_active then
-				meta:set_string("infotext","Furnace is empty")
-				swap_node(pos,"default:furnace")
-				meta:set_string("formspec", default.furnace_inactive_formspec)
-			end
-			return
-		end
-
-		meta:set_string("fuel_totaltime", fuel.time)
-		meta:set_string("fuel_time", 0)
-		
-		local stack = inv:get_stack("fuel", 1)
-		stack:take_item()
-		inv:set_stack("fuel", 1, stack)
-	end,
+minetest.register_craft({
+	output = "alchemtinker:aqua_plogh",
+	recipe = {
+		{"alchemtinker:aqua_essence", "alchemtinker:aqua_essence"},
+		{"", "alchemtinker:aqua_essence"},
+		{"", "alchemtinker:aqua_essence"}
+	}
 })
 
---]]
+-----Ignus Sword-----
+minetest.register_tool("alchemtinker:ignus_rapier", {
+    description = "Ignus Rapier",
+    inventory_image = "tinker_ignus_rapier.png",
+	tool_capabilities = {
+		full_punch_interval = 0.7,
+		max_drop_level=1,
+		groupcaps={
+			snappy={times={[1]=2.0, [2]=1.00, [3]=0.35}, uses=30, maxlevel=3},
+		},
+		damage_groups = {fleshy=7},
+	}
+})
 
+minetest.register_craft({
+	output = "alchemtinker:ignus_rapier",
+	recipe = {
+		{"alchemtinker:ignus_essence", "", ""},
+		{"", "alchemtinker:ignus_essence", ""},
+		{"", "", "alchemtinker:ignus_essence"}
+	}
+})
